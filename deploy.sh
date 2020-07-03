79f7ab1a2afd25cdc324db7a9c2dc4e43e620b43
EC2 Default User <ec2-user@ip-172-31-46-113.ap-northeast-2.compute.internal>
2020-07-03 22:05:55 +0900

    cache clear

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/project/NewSangBlog2
+PROJECT_NAME=new-sang-blog2
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> Git pull"
+git pull
+
+echo "> 프로젝트 Build 시작"
+./gradlew build
+
+echo "> NewSangBlog2 디렉토리 이동"
+cd $REPOSITORY
+
+echo "> build 파일 복사"
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+
+echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+	echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar \
+	-Dspring.config.location=classpath:/application.yml,/home/ec2-user/project/application-oauth.yml \
+	$REPOSITORY/$JAR_NAME 2>&1 &
