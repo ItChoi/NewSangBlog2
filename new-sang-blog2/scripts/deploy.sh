3a0815dd5f54c931e503edd729e52d64da41043d
itchoi <itchoi0429@gmail.com>
2020-07-14 01:45:22 +0900

    auto deploy test

--- new-sang-blog2/scripts/deploy.sh
+++ new-sang-blog2/scripts/deploy.sh
@@ -5,10 +5,12 @@ PROJECT_SCHEMA=NewSangBlog2
 PROJECT_NAME=new-sang-blog2
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.zip $REPOSITORY
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 PID 확인"
 CURRENT_PID=$(pgrep -fl NewSangBlog2 | grep jar | awk '{print $1}')
+
+echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
