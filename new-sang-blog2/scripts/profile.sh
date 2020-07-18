b9b64c89af29e93f8b73410d69db985979ddddbe
itchoi <itchoi0429@gmail.com>
2020-07-18 11:38:05 +0900

    무중단 배포 소스 수정

--- new-sang-blog2/scripts/profile.sh
+++ new-sang-blog2/scripts/profile.sh
@@ -2,7 +2,7 @@
 
 # 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있는 것
 function find_idle_profile() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}") http://localhost/profile)
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
   if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면, 40X, 50X 에러 모두 포함
   then
@@ -23,7 +23,7 @@ function find_idle_profile() {
 
 # 쉬고 있는 profile의 port 찾기
 function find_idle_port() {
-  IDLE_PROFILE=${find_idle_profile}
+  IDLE_PROFILE=$(find_idle_profile)
 
   if [ ${IDLE_PROFILE} == real1 ]
   then
