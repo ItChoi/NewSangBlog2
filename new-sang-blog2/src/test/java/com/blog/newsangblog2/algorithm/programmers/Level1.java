77f7f87484d6332c3028dabf7a85a2f38901ff91
itchoi <itchoi0429@gmail.com>
2020-07-22 22:01:35 +0900

    test ignore

--- new-sang-blog2/src/test/java/com/blog/newsangblog2/algorithm/programmers/Level1.java
+++ new-sang-blog2/src/test/java/com/blog/newsangblog2/algorithm/programmers/Level1.java
@@ -2,6 +2,12 @@ package com.blog.newsangblog2.algorithm.programmers;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 프로그래머스 알고리즘 문제 풀어보자.
  */
@@ -98,6 +104,37 @@ public class Level1 {
 
     // 체육복 private 함수 start
 
+
+    @Test
+    public void 알고리즘_문제() {
+        String q = "ab23c4d56e78f9g12h34i5j12k45l67n89m99o1k123p456q567r768s890t67u456v345w234x23y239z";
+        String regEx = "\\d{1,}+";
+
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(q);
+
+        List<Integer> list = new ArrayList();
+
+        while (matcher.find()) {
+            list.add(Integer.valueOf(matcher.group()));
+        }
+
+        Integer[] numbers = list.toArray(new Integer[list.size()]);
+
+        int minIndex = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[minIndex] > numbers[i]) {
+                minIndex = i;
+            }
+
+            if (numbers[maxIndex] < numbers[i]) {
+                maxIndex = i;
+            }
+        }
+
+        System.out.println("result: " + (minIndex + maxIndex));
+    }
 
 
 }
